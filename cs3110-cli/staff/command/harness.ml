2083b3424af408bee20515aef6abb679dbcc9013
bkc39 <bkc39@cornell.edu>
2014-06-12 03:08:56 -0400

    [cli] Finish helper functions for harness.

--- cs3110-cli/staff/command/harness.ml
+++ cs3110-cli/staff/command/harness.ml
@@ -1,7 +1,10 @@
+open Build
 open Constants
 open Core.Std
-open IOUtil
 open FilepathUtil
+open IOUtil
+open Postscript
+open Test
 
 (** [harness_collect_output rubric] reads the generated files for test
   output and test failures and organizes the results.  Points are
@@ -52,21 +55,120 @@ let harness_collect_output rubric : int * string list =
             end in
         (pts', msg :: strs))
 
-(* let initialize_spreadsheet () = *)
-(*   if Sys.file_exists cms_fname then *)
-(*     (\* Open existing file *\) *)
-(*     open_out_gen [Open_creat; Open_text; Open_append] 0o777 cms_fname *)
-(*   else *)
-(*     let cms_chn = open_out cms_fname in *)
-(*     output_string cms_chn "NetID"; *)
-(*     List.iter *)
-(*       (fun name -> *)
-(*        output_string cms_chn (Format.sprintf ",%s" (String.capitalize name))) *)
-(*       test_suite; *)
-(*     output_string cms_chn ",Add Comments\n"; *)
-(*     cms_chn *)
+(** Gets all of the test files in the given directory. *)
+let test_files_from_directory test_directory =
+  match Sys.is_directory test_directory with
+  | `Yes -> begin
+    Array.fold_right (Sys.readdir test_directory)
+                     ~init:[]
+                     ~f:(fun file acc -> strip_suffix file :: acc)
+  end
+  | `No
+  | `Unknown -> begin
+    let err_msg = Printf.sprintf "The test suite directory '%s' is invalid\n"
+                                 test_directory in
+    failwith err_msg
+  end
 
-(* let copy_tests () = failwith "TODO" *)
+let initialize_spreadsheet test_suite () =
+  match Sys.file_exists cms_fname with
+  | `Yes -> open_out_gen [Open_creat; Open_text; Open_append] 0o777 cms_fname
+  | `No
+  | `Unknown -> begin
+    let cms_chn = open_out cms_fname in
+    output_string cms_chn "NetID";
+    List.iter
+      ~f:(fun name ->
+          output_string cms_chn (Format.sprintf ",%s" (String.capitalize name)))
+      test_suite;
+    output_string cms_chn ",Add Comments\n";
+    cms_chn
+  end
+
+(** [print titles] prints a message to [text_channel] based off of the
+test [netid] *)
+let print_titles text_channel netid =
+  Format.printf "\n## Running tests for: %s ##\n%!" netid;
+  output_string text_channel
+                (Format.sprintf "## Automated test results for %s ##\n" netid)
+
+(** Returns an [in_channel] with a handle on a postscript file to
+record the test results of [net_id] on the test [test_name]. *)
+let prepare_postscript_channel netid test_name =
+  let fname =
+    Format.sprintf "%s/%s/%s-%s.ps" (Sys.getcwd ()) output_dir netid test_name in
+  let title = Format.sprintf "%s\t\t%s.ml" netid test_name in
+  ps_open_channel fname title
+
+(** Copies the source in [src_fname] and writes in to [postscript_channel]. *)
+let copy_source_to_postscript src_fname postscript_channel =
+  match Sys.file_exists src_fname with
+  | `Yes -> begin
+    output_string postscript_channel
+                  (Format.sprintf "Source code for file '%s':\n" src_fname);
+    ps_set_font postscript_channel ps_code_font;
+    List.iter ~f:(fun line -> output_string postscript_channel (line^"\n"))
+              (read_lines (open_in src_fname));
+    flush postscript_channel
+  end
+  | `No
+  | `Unknown -> begin
+    output_string postscript_channel "SOURCE NOT FOUND\n";
+    flush postscript_channel
+  end
+
+let build_and_run test_dir test_name rubric =
+  let _ = Sys.command (Format.sprintf "cp %s/%s.ml ." test_dir test_name)in
+  let score_and_output = ref (0,[]) in
+  let () =
+    build ~with_exit_code:(fun exit_code ->
+                           if exit_code <> 0 then
+                             score_and_output := 0, ["NO COMPILE"]
+                           else begin
+                             let _ = test_logging_errors test_name in
+                             score_and_output := harness_collect_output rubric
+                           end)
+          true
+          test_name
+          () in
+  !score_and_output
+
+let print_results (score,results) cms_channel postscript_channel text_channel =
+  (** Format the title *)
+  ps_set_font postscript_channel ps_header_font;
+  output_string postscript_channel "\nTest Results:\n";
+  ps_set_font postscript_channel ps_normal_font;
+  (** Print the results for each case. *)
+  List.iter ~f:(fun msg ->
+                print_endline msg;
+                output_string text_channel "    ";
+                output_string text_channel msg;
+                output_string text_channel "\n";
+                output_string postscript_channel msg;
+                output_string postscript_channel "\n")
+            results;
+  (* Print aggregate results to CMS *)
+  output_string cms_channel (Format.sprintf ",%d" score);
+  (* Flush and close postscript *)
+  flush postscript_channel;
+  flush text_channel;
+  Unix.close_process_out postscript_channel
+
+let remove_generated_files test_name =
+  let rm_test_output () = ignore (Sys.command ("rm " ^ test_output)) in
+  let rm_fail_output () = ignore (Sys.command ("rm " ^ fail_output)) in
+  (* 2014-01-19: Removing tests so they don't screw with reverse harness *)
+  ignore(Sys.command (Format.sprintf "rm %s.ml" test_name));
+  match Sys.file_exists test_output, Sys.file_exists fail_output with
+  | `Yes,`Yes         -> rm_test_output (); rm_fail_output ()
+  | `Yes,`No
+  | `Yes,`Unknown     -> rm_test_output ()
+  | `No,`Yes
+  | `Unknown,`Yes     -> rm_fail_output ()
+  | `No,`No
+  | `No,`Unknown
+  | `Unknown,`No
+  | `Unknown,`Unknown -> ()
 
 (* (\** [harness tests targets] run each set of unit tests under [tests] *)
 (*  * against [targets] *\) *)
