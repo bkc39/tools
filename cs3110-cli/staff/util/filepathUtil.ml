bbe08c39ecf2a06150498ee5164573eb6ab8dfff
bkc39 <bkc39@cornell.edu>
2014-06-12 05:16:20 -0400

    Add function to get subdirectories.

--- cs3110-cli/staff/util/filepathUtil.ml
+++ cs3110-cli/staff/util/filepathUtil.ml
@@ -9,23 +9,23 @@ let absolute_path (f : string) =
   Format.sprintf "%s/%s" (Sys.getcwd ()) f
 
 (** [split s c b] safely splits string [s] at the left/right-most occurence
- * of character [c]. [b] chooses. If [c] does not appear, returns two 
+ * of character [c]. [b] chooses. If [c] does not appear, returns two
  * copies of [s]. *)
-let split (s : string) (c : char) (b : bool) : string * string = 
+let split (s : string) (c : char) (b : bool) : string * string =
   if not (String.contains s c) then
     (s, s)
-  else 
+  else
     let i = 1 + if b then String.index s c else String.rindex s c in
     let left = String.sub s 0 (i-1) in
-    let right = String.sub s i ((String.length s) - i) in 
+    let right = String.sub s i ((String.length s) - i) in
     (String.trim left, String.trim right)
 
-let lsplit (s : string) (c : char) : string * string = 
+let lsplit (s : string) (c : char) : string * string =
   split s c true
 let rsplit (s : string) (c : char) : string * string =
   split s c false
 
-(** [strip_suffix str] strips all characters after and including the 
+(** [strip_suffix str] strips all characters after and including the
  * rightmost period (.) *)
 let strip_suffix (filename : string) : string =
   if (String.contains filename '.')
@@ -37,20 +37,20 @@ let strip_suffix (filename : string) : string =
 let strip_trailing_slash (s : string) =
   let len = String.length s in
   if s.[len-1] = '/'
-  then String.sub s 0 (len - 1) 
+  then String.sub s 0 (len - 1)
   else s
 
-(** [strip_trailing_slash_all strs] remove the trailing slash from a 
+(** [strip_trailing_slash_all strs] remove the trailing slash from a
  * list of files *)
-let strip_trailing_slash_all (directories : string list) = 
+let strip_trailing_slash_all (directories : string list) =
   List.map (fun s -> strip_trailing_slash s) directories
 
 (** [tag_of_path p] strips all characters up to and including the rightmost / *)
-let tag_of_path (path : string) = 
-  if String.contains path '/' then 
+let tag_of_path (path : string) =
+  if String.contains path '/' then
     let i = 1 + String.rindex path '/' in
     String.sub path i ((String.length path) - i)
-  else 
+  else
     path
 
 (** [assert_file_exists f] raises [File_not_found] if [f] does not exist *)
@@ -60,7 +60,7 @@ let assert_file_exists (filename : string) : unit =
 
 (** [ensure_dir d] creates the directory [d] if it does not exist already. *)
 let ensure_dir (dir_name : string) =
-  if not (Sys.file_exists dir_name) then 
+  if not (Sys.file_exists dir_name) then
     Unix.mkdir dir_name 0o777
 
 (**
@@ -72,7 +72,7 @@ let ensure_dir (dir_name : string) =
  * This is the same as
  *   > cs3110 smoke dir/a dir/b dir/c
  *)
-let directories_of_list (dir : string) : string list = 
+let directories_of_list (dir : string) : string list =
   let dir = strip_trailing_slash dir in
   let len = String.length dir in
   (* Remove the leading @ to get a filename *)
@@ -85,7 +85,7 @@ let directories_of_list (dir : string) : string list =
   (* Return the fully-inferred list of directories *)
   List.rev (List.fold_left (fun acc name -> (prefix ^ name) :: acc) [] dir_names)
 
-(** [test_name_of_line s] extract the test name from a line printed by the 
+(** [test_name_of_line s] extract the test name from a line printed by the
  * inline test runner. Name should be the last 'word' of the string, separated
  * from everything else by a colon *)
 let test_name_of_line (line : string) : string =
@@ -96,7 +96,7 @@ let test_name_of_line (line : string) : string =
     occuring to the right of the right-most occurence of the '.'
     character. *)
 let get_extension (file_name : string) : string option =
-  try 
+  try
     let start  = String.rindex file_name '.' + 1 in
     let length = String.length file_name - start in
     Some (String.sub file_name start length)
@@ -121,3 +121,17 @@ let get_files_with_extension (desired_extension : string)
     filter_by_extension desired_extension (Array.to_list (Sys.readdir dir)) in
   try get_files dir with _ -> []
 
+let get_subdirectories dir =
+  if Sys.is_directory (strip_trailing_slash dir)
+  then begin
+    Array.fold_right (fun fname dirs ->
+                      if Sys.is_directory (strip_trailing_slash fname)
+                      then fname::dirs
+                      else dirs)
+                     (Sys.readdir dir)
+                     []
+  end
+  else begin
+    let msg = Printf.sprintf "'%s' is not a valid directory!" dir in
+    failwith msg
+  end
